9db46e09e0c302a3b550a373aa36affef38e2781
Joseph Schenk <jcschenk8@gmail.com>
2021-03-25 15:58:07 +0000

    about page first iteration

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Skills from './Skills'
 import headshot from '../images/headshot.jpg'
+import selfPortrait from '../images/selfPortrait.jpg'
+import climbing from '../images/climbing.jpg'
+import motorbike from '../images/motorbike.jpg'
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel'
+import 'pure-react-carousel/dist/react-carousel.es.css'
 
 
-import AwesomeSlider from 'react-awesome-slider'
-import AwesomeSliderStyles from 'react-awesome-slider/src/styles';
 
 export default function AboutMe() {
   return <section className="hero is-fullheight" id="about">
@@ -12,10 +15,35 @@ export default function AboutMe() {
       <div className="container has-text-centered">
         <div className="columns">
 
-          <div className="column">
+          <div className="box column">
+
+            <p className="title">About Me</p>
             <p>
-              Brand Statement Here
-            </p>
+              I am a junior software engineer with business experience and a first class honours degree in mechanical engineering. Alongside my degree, I continued to run my own successful photography business and technical advice service in the climbing industry.
+            </p><br></br>
+            <p>
+              At university, I trained in Matlab and completed a thesis on the use of Software for Computer Vision. I decided to pursue a career in Software Engineering and enrolled in an immersive course at General Assembly.
+            </p><br></br>
+            <p>
+              Trained in multiple coding languages and best practices I am looking to gain a role as a full stack developer. I am hard working, a quick learner and a great team player. I look forward to growing my experience and technical skills within a fast-paced environment.
+            </p><br></br>
+            <p>Outside of coding I maintain my skills as a professional photographer and enjoy going climbing and motorcycling</p>
+            <br></br>
+            <p>If you'd like to get in touch, please <a href="#contact">contact me</a> below</p><br></br>
+            <div className="has-text-centered">
+              <CarouselProvider
+                naturalSlideWidth={25}
+                naturalSlideHeight={10}
+                totalSlides={3}>
+                <Slider>
+                  <Slide index={0}><img className="aboutImage" src={selfPortrait}></img></Slide>
+                  <Slide index={1}><img className="aboutImage" src={climbing}></img></Slide>
+                  <Slide index={2}><img className="aboutImage" src={motorbike}></img></Slide>
+                </Slider>
+                <ButtonBack className="button m-1">Back</ButtonBack>
+                <ButtonNext className="button m-1">Next</ButtonNext>
+              </CarouselProvider>
+            </div>
           </div>
           <div className="column">
             <div className="box">
@@ -25,7 +53,7 @@ export default function AboutMe() {
         </div>
       </div>
     </div>
-  
+
     <Skills />
   </section>
 }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,17 +1,71 @@
 import React from 'react'
+// import AwesomeSlider from 'react-awesome-slider'
+// // import AwesomeSliderStyles from 'react-awesome-slider/src/styles';
+// import 'react-awesome-slider/dist/styles.css'
 
 export default function Skills() {
 
-  return <div className="columns">
-    <div className="column">
-      <i className="devicon-html5-plain"></i>
-      <i className="devicon-css3-plain"></i>
-      <i className="devicon-javascript-plain"></i>
-    </div>
-    <div className="column">
-      <i className="devicon-react-original"></i>
-      <i className="devicon-sass-original"></i>
-      <i className="devicon-nodejs-plain"></i>
+  return <div className="has-text-centered">
+    <p className="sub-title">Technical Skills</p>
+    <div className="columns">
+      <div className="column is-one-fifth"></div>
+      <div className="column">
+        <div className="columns is-mobile is-desktop">
+          <div className="column">
+            <div className="iconDiv"><i className="icon devicon-babel-plain"></i><p>Babel</p></div>
+            <div className="iconDiv"><i className="icon devicon-github-original"></i><p>Github</p></div>
+            <div className="iconDiv"><i className="icon devicon-mongodb-plain"></i><p>MongoDB</p></div>
+            <div className="iconDiv"><i className="icon devicon-postgresql-plain"></i><p>PostgreSQL</p></div>
+            <div className="iconDiv"><i className="icon devicon-slack-plain"></i><p>Slack</p></div>
+          </div>
+          <div className="column">
+            <div className="iconDiv"><i className="icon devicon-chrome-plain"></i><p>Chrome</p></div>
+            <div className="iconDiv"><i className="icon devicon-heroku-original"></i><p>Heroku</p></div>
+            <div className="iconDiv"><i className="icon devicon-matlab-plain"></i><p>Matlab</p></div>
+            <div className="iconDiv"><i className="icon devicon-python-plain"></i><p>Python</p></div>
+            <div className="iconDiv"><i className="icon devicon-sqlalchemy-plain"></i><p>SQLAlchemy</p></div>
+          </div>
+          <div className="column">
+            <div className="iconDiv"><i className="icon devicon-express-original"></i><p>Express</p></div>
+            <div className="iconDiv"><i className="icon devicon-html5-plain"></i><p>HTML5</p></div>
+            <div className="iconDiv"><i className="icon devicon-nodejs-plain"></i><p>NodeJS</p></div>
+            <div className="iconDiv"><i className="icon devicon-react-original"></i><p>ReactJS</p></div>
+            <div className="iconDiv"><i className="icon devicon-ssh-original"></i><p>SSH</p></div>
+          </div>
+          <div className="column">
+            <div className="iconDiv"><i className="icon devicon-flask-original"></i><p>Flask</p></div>
+            <div className="iconDiv"><i className="icon devicon-javascript-plain"></i><p>JavaScript</p></div>
+            <div className="iconDiv"><i className="icon devicon-npm-original-wordmark"></i><p>NPM</p></div>
+            <div className="iconDiv"><i className="icon devicon-safari-plain"></i><p>Safari</p></div>
+            <div className="iconDiv"><i className="icon devicon-visualstudio-plain"></i><p>VisualStudio</p></div>
+          </div>
+          <div className="column">
+            <div className="iconDiv"><i className="icon devicon-git-plain"></i><p>Git</p></div>
+            <div className="iconDiv"><i className="icon devicon-mocha-plain"></i><p>Mocha</p></div>
+            <div className="iconDiv"><i className="icon devicon-photoshop-plain"></i><p>PhotoShop</p></div>
+            <div className="iconDiv"><i className="icon devicon-sass-original"></i><p>Sass</p></div>
+            <div className="iconDiv"><i className="icon devicon-webpack-plain"></i><p>Webpack</p></div>
+          </div>
+        </div>
+
+
+
+
+
+
+
+      </div>
+      <div className="column is-one-fifth"></div>
     </div>
   </div>
+
+
+
+
+
 }
+
+<div className="columns">
+
+
+</div>
