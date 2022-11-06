de854a4f7d9cc133cfff4bc204f42691a85d21f2
Joseph Schenk <jcschenk8@gmail.com>
2022-11-06 20:30:43 +0100

    fix(photography placeholder): added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,5 @@
 import React from 'react'
 import LandingPage from './LandingPage'
-import AboutMe from './AboutMe'
-import Rigaud from './Rigaud'
-import Picster from './Picster'
-import ParkPong from './ParkPong'
-import FilmFinder from './FilmFinder'
-import ZombieSweeper from './ZombieSweeper'
-import Contact from './Contact'
 
 
 export default function Home() {
@@ -15,18 +8,5 @@ export default function Home() {
   return <div>
     <LandingPage />
     <div className="level"></div>
-    <AboutMe />
-    <div className="level"></div>
-    <Rigaud />
-    <div className="level"></div>
-    <Picster />
-    <div className="level"></div>
-    <ParkPong />
-    <div className="level"></div>
-    <FilmFinder />
-    <div className="level"></div>
-    <ZombieSweeper />
-    <div className="level"></div>
-    <Contact />
   </div>
 }
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,19 +1,31 @@
 import React from 'react'
-import scroll from '../images/scroll.png'
-import Navbar from './Navbar'
+import { useState } from 'react'
+import { HiOutlineMail } from 'react-icons/hi'
+import { IconContext } from 'react-icons/lib'
+import { AiOutlineInstagram } from 'react-icons/ai'
 
 export default function LandingPage() {
-  return <section className="hero is-fullheight landing-image">
-    <Navbar />
-    <div className="hero-body">
-      <div className="container has-text-centered">
-        <p className="title has-text-white is-size-1">Joseph Schenk</p>
-        <p className="subtitle has-text-white is-size-4">Software Engineer</p>
+  const [copied, updateCopied] = useState(false)
+  function copyEmail() {
+    navigator.clipboard.writeText('jcschenk8@gmail.com')
+    updateCopied(true)
+  }
 
+  return <section className="hero is-fullheight landing-image">
+    <div className="hero-body is-flex-direction-column"  id="modal-background">
+      <div className="container has-text-centered is-flex is-flex-direction-column is-justify-content-center">
+        <p className="title has-text-white is-size-1">Joseph Schenk Photography</p>
+        <p className="subtitle has-text-white is-size-4">Coming soon...</p>
       </div>
-    </div>
-    <div className="hero-foot has-text-centered">
-      <img className="fade" id="scroll" src={scroll}></img>
+      <div className="has-text-centered">
+              <IconContext.Provider value={{ color: 'white' }}>
+                <button onClick={copyEmail} className="button m-0 is-medium is-primary"><HiOutlineMail /></button>
+                <a href="https://www.instagram.com/josephschenk_photography/" target="blank"><button className="button m-0 is-medium is-primary"><AiOutlineInstagram /></button></a>
+                <div>{!copied && <small className="text-holder">Email copied to clipboard</small>}</div>
+                <div>{copied && <small className="has-text-white">Email copied to clipboard</small>}</div>
+              </IconContext.Provider>
+            </div>
+
     </div>
   </section>
 }
