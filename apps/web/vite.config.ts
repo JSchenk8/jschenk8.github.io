088798bd6a38692564651596e01a90abf7754bae
Joseph Schenk <jcschenk8@gmail.com>
2026-06-27 20:18:46 +0200

    vite config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,12 +4,12 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 // https://vite.dev/config/
-export default defineConfig(({command}) => ({
-  base: command === "build" ? "/jschenk8.github.io/" : "/",
+export default defineConfig({
+  base: "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-}))
+  }
+})
